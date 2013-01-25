cb6be3fb8dc33ccf27cdc7f09b05539e7602417f
Ryan Smith <0ryansmith1994@gmail.com>
2013-01-25 11:17:47 +0000

    Created Classes required by Marina

---
+++ src/classes.py
@@ -0,0 +1,75 @@
+class Lease(object):
+    """Stores information about a Lease."""
+    def __init__(self, customer, boat, start, end, package):
+        """Creates a new lease instance."""
+        self._customer = customer
+        self._boat = boat
+        self._start = start
+        self._end = end
+        self._package = package
+
+
+class User(object):
+    """Stores information about a user."""
+    def __init__(self, name):
+        self._name = name
+        self._contact = {
+            "address": "",
+            "postcode": "",
+            "phoneNumber": ""
+        }
+
+    def setName(self, name):
+        self._name = name
+
+    def setAddress(self, address, postcode):
+        self._contact["address"] = address
+        self._contact["postcode"] = postcode
+
+    def setPhone(self, phoneNumber):
+        self._contact["phoneNumber"] = phoneNumber
+
+    def getName(self):
+        return self._name
+
+    def getAddress(self):
+        return self._address + self._postcode
+
+    def getPhone(self):
+        return self._phone
+
+
+class Customer(User):
+    """Stores information about a customer."""
+    def __init__(self, name):
+        super(Customer, self).__init__(name)
+        self._boats = []
+
+    def addBoat(self, boat):
+        self._boats.append(boat)
+
+    def removeBoat(self, boat):
+        boats = self._boats
+        boats.pop(boats.index(boat))
+
+    def getBoats(self):
+        return self._boats
+
+
+class Boat(object):
+    """Stores information about a boat."""
+    def __init__(self, name, description):
+        self._name = name
+        self._description = description
+
+    def setName(self, name):
+        self._name = name
+
+    def setDescription(self, description):
+        self._description = description
+
+    def getName(self):
+        return self._name
+
+    def getDescription(self):
+        return self._description
