0d273cfb42fba59ebf2fbaf645a0a6f1efd8d000
Ryan Smith <0ryansmith1994@gmail.com>
2013-03-06 15:17:29 +0000

    Changed import to avoid confusion.
    I've changed the line that imports the code so that it is explicit what
    is being called from the tkinter module throughout the code to avoid
    confusion. This may be more important later on when we are importing
    multiple modules.

--- src/login.py
+++ src/login.py
@@ -1,21 +1,25 @@
-from tkinter import *
+import tkinter
+
 
 def login():
-    win = Toplevel()
+    win = tkinter.Toplevel()
     win.title("success")
     root.withdraw()
     makeLogin(win)
 
+
 def register():
     pass
 
-def makeLogin(window):
-    Label(window, text="Enter ID").grid(row=0, column=0, columnspan=2)
-    Entry(window).grid(row=1, column=0, columnspan=2)
-    Button(window, text="Login", command=login).grid(row=2, column=0)
-    Button(window, text="Register", command=register).grid(row=2, column=1)
 
-root=Tk()
+def makeLogin(window):
+    tkinter.Label(window, text="Enter ID").grid(row=0, column=0, columnspan=2)
+    tkinter.Entry(window).grid(row=1, column=0, columnspan=2)
+    tkinter.Button(window, text="Login", command=login).grid(row=2, column=0)
+    tkinter.Button(window, text="Register", command=register).grid(row=2, column=1)
+
+
+root = tkinter.Tk()
 root.title("Login")
 makeLogin(root)
 root.mainloop()
